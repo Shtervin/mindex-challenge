30364db1a85cf18788999e1f8c2c46b33eeab7dc
Shtervin <rajrajwade867@gmail.com>
2023-03-03 10:31:02 -0500

    added few UI changes and unit tests for 2 components

--- src/app/delete-pop-up/delete-pop-up.component.spec.ts
+++ src/app/delete-pop-up/delete-pop-up.component.spec.ts
@@ -1,17 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DeletePopUpComponent } from './delete-pop-up.component';
 
 describe('DeletePopUpComponent', () => {
   let component: DeletePopUpComponent;
   let fixture: ComponentFixture<DeletePopUpComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DeletePopUpComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [DeletePopUpComponent],
+      imports: [MatDialogModule],
+      providers: [
+        { provide: MatDialogRef, useValue: {close: () => {}} },
+        { provide: MAT_DIALOG_DATA, useValue: {title: "DELETE DIRECT REPORT"} },
+      ],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DeletePopUpComponent);
@@ -22,4 +26,50 @@ describe('DeletePopUpComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the title', () => {
+    expect(fixture.nativeElement.querySelector('h2').textContent).toContain("DELETE DIRECT REPORT");
+  });
+
+  it('should display the OK button with the correct text', () => {
+    const okButton = fixture.nativeElement.querySelector('.ok-button');
+    expect(okButton).toBeTruthy();
+    expect(okButton.textContent).toContain("Ok");
+  });
+
+  it('should display the Cancel button with the correct text', () => {
+    const cancelButton = fixture.nativeElement.querySelector('.cancel-button');
+    expect(cancelButton).toBeTruthy();
+    expect(cancelButton.textContent).toContain("Cancel");
+  });
+
+  it('should call onOk on Ok button click', () => {
+    spyOn(component, "onOk");
+    const okButton = fixture.nativeElement.querySelector('.ok-button');
+    okButton.click();
+    expect(component.onOk).toHaveBeenCalled();
+  });
+
+  it('should call onCancel on Cancel button click', () => {
+    spyOn(component, 'onCancel');
+    const cancelButton = fixture.nativeElement.querySelector('.cancel-button');
+    cancelButton.click();
+    expect(component.onCancel).toHaveBeenCalled();
+  });
+
+  it('should emit confirmed event when Ok button is clicked', () => {
+    spyOn(component.onOkEmitter, 'emit');
+    spyOn(component.dialogRef, 'close');
+    component.onOk()
+    expect(component.onOkEmitter.emit).toHaveBeenCalled();
+    expect(component.dialogRef.close).toHaveBeenCalled();
+  })
+
+  it('should emit event when Cancel button is clicked', () => {
+    spyOn(component.onOkEmitter, 'emit');
+    spyOn(component.dialogRef, 'close');
+    component.onCancel()
+    expect(component.onOkEmitter.emit).toHaveBeenCalled();
+    expect(component.dialogRef.close).toHaveBeenCalled();
+  })
 });
--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,8 +1,9 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, TestBed, ComponentFixture} from '@angular/core/testing';
 import {Component, Input} from '@angular/core';
 
 import {EmployeeListComponent} from './employee-list.component';
 import {EmployeeService} from '../employee.service';
+import { Observable, of } from 'rxjs';
 
 @Component({selector: 'app-employee', template: ''})
 class EmployeeComponent {
@@ -20,6 +21,36 @@ class GridTileComponent {
 const employeeServiceSpy = jasmine.createSpyObj('EmployeeService', ['getAll', 'get', 'save', 'remove']);
 
 describe('EmployeeListComponent', () => {
+  let component: EmployeeListComponent;
+  let fixture: ComponentFixture<EmployeeListComponent>;
+  const data$ = of([
+    {
+      id: 1,
+      firstName: 'Brian',
+      lastName: 'McGee',
+      position: 'CEO',
+      directReports: [2, 3]
+    },
+    {
+      id: 2,
+      firstName: 'Homer',
+      lastName: 'Thompson',
+      position: 'Dev Manager',
+      directReports: [4]
+    },
+    {
+      id: 3,
+      firstName: 'Rock',
+      lastName: 'Strongo',
+      position: 'Lead Tester'
+    },
+    {
+      id: 4,
+      firstName: 'Max',
+      lastName: 'Power',
+      position: 'Junior Software Engineer'
+    }
+  ]);
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -29,14 +60,37 @@ describe('EmployeeListComponent', () => {
         GridTileComponent
       ],
       providers: [
-        {provide: EmployeeService, useValue: employeeServiceSpy}
+        {provide: EmployeeService, useValue: {getAll: () => data$}}
       ],
     }).compileComponents();
+
+
   }));
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeeListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
 
   it('should create the component', async(() => {
-    const fixture = TestBed.createComponent(EmployeeListComponent);
-    const comp = fixture.debugElement.componentInstance;
-    expect(comp).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
+
+  it('should create total reports', async(() => {
+    expect(component.allReports.length).toBeGreaterThan(0);
+    expect(component.allReports.length).toBe(4);
+
+  }))
+
+  it('should delete direct report if it exists', async(() => {
+    expect(component.allReports[0]["directReports"].length).toBe(2);
+    component.handleDeletedEvent([1, 2]);
+    expect(component.allReports[0]["directReports"].length).toBe(1);
+  }))
+
+  it('should update compensation at the correct employee id', async(() => {
+    component.handleUpdatedCompensation([1, 15000]);
+    expect(component.employees[0]["compensation"]).toBe(15000);
+  }))
 });
